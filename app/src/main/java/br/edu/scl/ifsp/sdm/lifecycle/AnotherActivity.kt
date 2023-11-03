9f4a2b9445c4bdfef90f7cc912d2c20ba9e8929a
aline <linegcruz@gmail.com>
2023-11-03 16:44:04 -0300

    Lifecycle study to understand Activity states, methods and life cycles.

--- app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/AnotherActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/AnotherActivity.kt
@@ -1,10 +1,12 @@
 package br.edu.scl.ifsp.sdm.lifecycle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.sdm.lifecycle.databinding.ActivityAnotherBinding
 
 class AnotherActivity : AppCompatActivity() {
+
     private val activityAnotherBinding: ActivityAnotherBinding by lazy {
         ActivityAnotherBinding.inflate(layoutInflater)
     }
@@ -12,5 +14,40 @@ class AnotherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityAnotherBinding.root)
+
+        setSupportActionBar(activityAnotherBinding.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.another)
+
+        Log.v(getString(R.string.app_name), "Another - onCreate(): início COMPLETO")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(getString(R.string.app_name), "Another - onStart(): início VISÍVEL")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(getString(R.string.app_name), "Another - onResume(): início PRIMEIRO PLANO")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(getString(R.string.app_name), "Another - onPause(): fim PRIMEIRO PLANO")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(getString(R.string.app_name), "Another - onStop(): fim VISÍVEL")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(getString(R.string.app_name), "Another - onDestroy(): fim COMPLETO")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(getString(R.string.app_name), "Another - onRestart(): preparando onStart()")
     }
 }
